671ef5cdf6de82a5fc48d802fb3235c33530619e
Kuchta Rafał <RKuchta@multikino.grupa.iti>
2019-03-25 21:37:20 +0100

    Updates

---
+++ precache-manifest.80d5b1254b1cd7649e10985351b9c26c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0ac7b5f58ca91b367b0af2a4270875e",
+    "url": "/React-router-app/static/media/header3.b0ac7b5f.jpg"
+  },
+  {
+    "revision": "fa2cae8d94161c276d375c70a57cfc79",
+    "url": "/React-router-app/static/media/header2.fa2cae8d.jpg"
+  },
+  {
+    "revision": "6f6dd185d6277823de4d02cbd8a89018",
+    "url": "/React-router-app/static/media/header1.6f6dd185.jpg"
+  },
+  {
+    "revision": "aef646e4b880d9e9947b",
+    "url": "/React-router-app/static/js/runtime~main.aef646e4.js"
+  },
+  {
+    "revision": "384f25b3a697c6deb2fa",
+    "url": "/React-router-app/static/js/main.384f25b3.chunk.js"
+  },
+  {
+    "revision": "d5934c58bb71e865b2f8",
+    "url": "/React-router-app/static/js/2.d5934c58.chunk.js"
+  },
+  {
+    "revision": "384f25b3a697c6deb2fa",
+    "url": "/React-router-app/static/css/main.3703f0ac.chunk.css"
+  },
+  {
+    "revision": "fd905bbdcc81bcd1a3cd91af440e9619",
+    "url": "/React-router-app/index.html"
+  }
+];
